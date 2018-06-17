f05c533a584d4a3694c96faca9478c0e98099b56
Bruno Reis <bfreis@gmail.com>
2018-06-17 00:35:07 -0700

    changing how GNU Coreutils are accessed, and prioritizing Homebrew's OpenSSL on $PATH

--- modules/system/aliases.zsh
+++ modules/system/aliases.zsh
@@ -1,30 +1,34 @@
-# configure all coreutils aliases
-if (( $+commands[brew] )); then
-  bindir=$(brew --prefix)/opt/coreutils/bin
-  if [[ -d $bindir ]]; then
-    for binary in $bindir/g*(*); do
-      alias_name=${binary#$bindir/g}
-      alias $alias_name=$binary
-    done
-  fi
-fi
+## configure all coreutils aliases
+#if (( $+commands[brew] )); then
+#  bindir=$(brew --prefix)/opt/coreutils/bin
+#  if [[ -d $bindir ]]; then
+#    for binary in $bindir/g*(*); do
+#      alias_name=${binary#$bindir/g}
+#      alias $alias_name=$binary
+#    done
+#  fi
+#fi
+#
+## configure all findutils aliases
+#if (( $+commands[brew] )); then
+#  bindir=$(brew --prefix)/opt/findutils/bin
+#  if [[ -d $bindir ]]; then
+#    for binary in $bindir/g*(*); do
+#      alias_name=${binary#$bindir/g}
+#      alias $alias_name=$binary
+#    done
+#  fi
+#fi
+#
+#if (( $+commands[gls] )); then
+#  alias l="ls -lAh --color"
+#  alias ll="ls -lAhF --color"
+#  alias la='ls -A --color'
+#fi
+alias l="ls -lAh --color"
+alias ll="ls -lAhF --color"
+alias la='ls -A --color'
 
-# configure all findutils aliases
-if (( $+commands[brew] )); then
-  bindir=$(brew --prefix)/opt/findutils/bin
-  if [[ -d $bindir ]]; then
-    for binary in $bindir/g*(*); do
-      alias_name=${binary#$bindir/g}
-      alias $alias_name=$binary
-    done
-  fi
-fi
-
-if (( $+commands[gls] )); then
-  alias l="ls -lAh --color"
-  alias ll="ls -lAhF --color"
-  alias la='ls -A --color'
-fi
 
 # GRC - Generic Couloriser
 # (must load after other aliases to proper colorize GNU coreutils and ...)
--- modules/system/path.zsh
+++ modules/system/path.zsh
@@ -1,1 +1,1 @@
-export PATH="$ZSH/bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH"
+export PATH="$ZSH/bin:$HOME/bin:$(brew --prefix)/opt/coreutils/libexec/gnubin:$(brew --prefix)/opt/openssl/bin:/usr/local/bin:/usr/local/sbin:$PATH"
