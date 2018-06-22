20d1abc5ecfbb131d889dd2c1ceb5b015476ee9b
Bruno Reis <bfreis@gmail.com>
2018-06-21 17:18:32 -0700

    improving path configuration, brew was failing in some systems

--- modules/system/path.zsh
+++ modules/system/path.zsh
@@ -1,1 +1,14 @@
-export PATH="$ZSH/bin:$HOME/bin:$(brew --prefix)/opt/coreutils/libexec/gnubin:$(brew --prefix)/opt/openssl/bin:/usr/local/bin:/usr/local/sbin:$PATH"
+typeset -U path
+
+path=('/usr/local/bin' $path)
+
+path=(
+  $HOME/bin
+  $ZSH/bin
+  $HOME/go/bin
+  $(brew --prefix)/opt/coreutils/libexec/gnubin
+  $(brew --prefix)/opt/openssl/bin
+  $path
+)
+
+export PATH
